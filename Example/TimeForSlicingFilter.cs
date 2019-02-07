807325096aaaf7b27f4693547a780bc46b30967b
kenamis <kenan.chabuk@gmail.com>
2019-02-06 18:28:48 -0800

    added additional directive for newer unity version than 5

--- Example/TimeForSlicingFilter.cs
+++ Example/TimeForSlicingFilter.cs
@@ -28,7 +28,7 @@ namespace NobleMuffins.LimbHacker.Examples
 			{
 				if (Input.GetKey(KeyCode.A)) return true;
 				if (isAlwaysTimeForSlicing) return true;
-#if UNITY_5
+#if UNITY_5 || UNITY_5_3_OR_NEWER //Starting with 5.3 we can use UNITY_X_Y_OR_NEWER
 				return animator != null && animator.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash(nameOfAnimationStateForSlicing);
 #else
 				return animator != null && animator.GetCurrentAnimatorStateInfo(0).nameHash == Animator.StringToHash(nameOfAnimationStateForSlicing);
